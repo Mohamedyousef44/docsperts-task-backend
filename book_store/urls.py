44f94b61d59b22ff282ad384a2ee033046dfdf7a
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-07-01 12:54:12 +0300

    add url for it

--- book_store/urls.py
+++ book_store/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("book.api.urls")),
     path("user/", include("user.urls")),
+    path("api/schema/", get_schema_view(title="Book API"), name="api_schema"),
+    path(
+        "api/docs/",
+        TemplateView.as_view(
+            template_name="docs.html", extra_context={"schema_url": "api_schema"}
+        ),
+        name="swagger-ui",
+    ),
 ]
