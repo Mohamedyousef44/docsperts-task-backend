fad865d055079977f7a4e4a7cab06890c73339bd
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-07-01 12:53:57 +0300

    add django-rest-swagger to our setting file to create api docs

--- book_store/settings.py
+++ book_store/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "user",
     "book",
+    "rest_framework_swagger",
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ ROOT_URLCONF = "book_store.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
